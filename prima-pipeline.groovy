faeca76d54e5a2641ddb23325816fde6727966fc
Andrea Colleoni <andrea@colleoni.info>
2021-07-30 15:38:55 +0200

    build parallel

--- prima-pipeline.groovy
+++ prima-pipeline.groovy
@@ -1,3 +1,7 @@
+def getWebappZipFile() {
+    return 'frontend-webapp.zip'
+}
+
 pipeline {
     agent any
 
@@ -15,26 +19,75 @@ pipeline {
                 bat 'dir'
             }
         }
-        stage('Compilazione') {
-            steps {
-                echo 'Compilazione maven'
-                dir('eclipse/devops-01') {
-                    withMaven(jdk: 'JDK 11', maven: 'Maven 3.8.1') {
-                        bat 'mvn compile'
+        stage('Build') {
+            parallel {
+                stage('Build backend') {
+                    steps {
+                        echo 'Compilazione maven'
+                        dir('eclipse/devops-01') {
+                            withMaven(jdk: 'JDK 11', maven: 'Maven 3.8.1') {
+                                bat 'mvn compile'
+                            }
+                        }                
+                        
+                    }
+                }
+                stage('Build frontend') {
+                    steps {
+                        dir('frontend/webapp') {
+                            withNPM(npmrcConfig: 'npm-config-base') {
+                                bat 'npm install'
+                                /*
+                                bat 'npm install @angular/cli'
+                                bat 'ng build'
+                                */
+                                bat 'npm run build'
+                            }
+                        }
                     }
                 }                
-                
             }
         }
-        stage('Testing') {
-            steps {
-                dir('eclipse/devops-01') {
-                    withMaven(jdk: 'JDK 11', maven: 'Maven 3.8.1') {
-                        bat 'mvn test'
+        stage('Test & package') {
+            parallel {
+                stage('Testing & Packaging backend') {
+                    steps {
+                        dir('eclipse/devops-01') {
+                            withMaven(jdk: 'JDK 11', maven: 'Maven 3.8.1') {
+                                bat 'mvn package'
+                            }
+                        }                
+                        
+                    }
+                }
+                stage('Packaging frontend') {
+                    steps {
+                        zip zipFile: "$webappZipFile", 
+                            archive: true, 
+                            dir: 'frontend/webapp/dist/webapp',
+                            overwrite: true
                     }
                 }                
-                
             }
-        }        
+        }
+    }
+    
+    post {
+        success {
+            bat "copy $webappZipFile c:\\Publish\\"
+        }
+        failure {
+            /*
+            emailext (
+                subject: "Build ${env.BUILD_NUMBER} del progetto ${env.JOB_NAME} fallita",
+                body: "<h2>Job ${env.BUILD_NUMBER} è fallilto alle ore ###</h2>",
+                to: 'andrea@colleoni.info'
+                )
+            */
+            echo 'Failure!!'
+        }
+        always {
+            echo 'Finish'
+        }
     }
 }
