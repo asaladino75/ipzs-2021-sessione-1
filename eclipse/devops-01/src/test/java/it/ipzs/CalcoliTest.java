c3a2be5b9a6bbdaa266a876125b5105a0791c9b9
Andrea Colleoni <andrea@colleoni.info>
2021-07-30 11:35:25 +0200

    script prima pipeline

--- eclipse/devops-01/src/test/java/it/ipzs/CalcoliTest.java
+++ eclipse/devops-01/src/test/java/it/ipzs/CalcoliTest.java
@@ -9,39 +9,39 @@ public class CalcoliTest {
 	@Test
 	public void testSomma() {
 		Calcoli c = new Calcoli();
-		
+
 		int a = 5;
 		int b = 15;
 		int expected = 20;
-		
+
 		int actual = c.somma(a, b);
-		
+
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testProdotto() {
 		Calcoli c = new Calcoli();
-		
+
 		int a = 5;
 		int b = 15;
 		int expected = 75;
-		
+
 		int actual = c.prodotto(a, b);
-		
+
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testQuoziente() {
-Calcoli c = new Calcoli();
-		
+		Calcoli c = new Calcoli();
+
 		int a = 15;
 		int b = 6;
 		int expected = 2;
-		
+
 		int actual = c.quoziente(a, b);
-		
+
 		assertEquals(expected, actual);
 	}
 
---
+++ prima-pipeline.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+        stage('GIT checkout') {
+            steps {
+                git branch: 'main', 
+                    url: 'https://github.com/andrea-colleoni/ipzs-2021-sessione-1.git'
+                    
+                bat 'dir'
+            }
+        }
+        stage('Compilazione') {
+            steps {
+                echo 'Compilazione maven'
+                dir('eclipse/devops-01') {
+                    withMaven(jdk: 'JDK 11', maven: 'Maven 3.8.1') {
+                        bat 'mvn clean'
+                    }
+                }                
+                
+            }
+        }
+    }
+}
