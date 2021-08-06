147ff02294ecae6a2cda3ca8e1c856389276cf29
asaladino75 <antonino.saladino@gmail.com>
2021-08-06 10:47:49 +0200

    modificato il messaggio

--- devops/eclipse/devops-01/src/main/java/it/ipzs/Hello.java
+++ devops/eclipse/devops-01/src/main/java/it/ipzs/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) throws InterruptedException {
 		while(true) {
 			Thread.sleep(10000);
-			System.out.println("Hello docker???");
+			System.out.println("Hello docker!");
 		}
 	}
 
